7542da83c02cdb04161e44e393c1363032a94ef1
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-17 18:35:31 +0300

    Hot Fix

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,12 +2,10 @@
 """
 Insert a document in Python
 """
-import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    insert a documents into a collection
-    """
-    data = mongo_collection.insert_one(kwargs)
-    return data.
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
