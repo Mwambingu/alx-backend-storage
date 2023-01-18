2c50ee87aef87ae2af6878031cb46893ba11c3ce
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-18 19:12:04 +0300

    Hot fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,16 +8,18 @@ from typing import Union
 
 
 class Cache:
-    """Represents a Redis DB object"""
+    '''Represents an object for storing data in a Redis data storage.
+    '''
 
     def __init__(self) -> None:
-        """Instantiates a Redis DB object"""
+        '''Initializes a Cache instance.
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
-        """Takes args data sets in redisdb with a unique
-        uuid and returns a string 'key' of the uuid"""
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
