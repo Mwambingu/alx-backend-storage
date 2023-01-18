b8346e4a0f2481221a674e85ab3488f368dbb4ca
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-18 19:03:32 +0300

    Hot Fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,19 +7,17 @@ import uuid
 from typing import Union
 
 
-class Cache:
-    '''Represents an object for storing data in a Redis data storage.
-    '''
+class Cache():
+    """Represents a Redis DB object"""
 
     def __init__(self) -> None:
-        '''Initializes a Cache instance.
-        '''
+        """Instantiates a Redis DB object"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Stores a value in a Redis data storage and returns the key.
-        '''
-        data_key = str(uuid.uuid4())
-        self._redis.set(data_key, data)
-        return data_key
+    def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Takes args data sets in redisdb with a unique
+        uuid and returns a string 'key' of the uuid"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
